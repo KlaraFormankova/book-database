c914adac6264ca98e1b64730833cb504d89dc217
Klara Formankova <xforma14@vutbr.cz>
2023-09-23 16:48:53 +0200

    Simple static books page

---
+++ src/app/admin/books/page.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+type Book = {
+    id: number;
+    title: string;
+    author: string;
+  };
+
+export default function Page() {
+    const books: Book[] = [
+        { id: 1, title: "Book 1", author: "Author 1" },
+        { id: 2, title: "Book 2", author: "Author 2" },
+        { id: 3, title: "Book 3", author: "Author 3" },
+    ];
+
+    return (
+        <div>
+            <Typography variant="h2" gutterBottom>
+                Books
+                <Link href="/admin/books/new"> <AddCircleOutlineIcon /> </Link>
+            </Typography>
+            <br />
+            <br />
+            <Grid container spacing={2}>
+                {books.map((book) => (
+                    <Grid key={book.id} item xs={6} md={4} lg={3}>
+                        <BookPaper book={book}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    )
+}
+
+
+function BookPaper(props: { book: Book }) {
+    return (
+        <Link href={`/admin/books/${props.book.id}`}>
+            <Paper elevation={3}>
+                <Typography variant="h5" gutterBottom>
+                    {props.book.title}
+                </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                    {props.book.author}
+                </Typography>
+            </Paper>
+        </Link>
+    );
+  }
+
