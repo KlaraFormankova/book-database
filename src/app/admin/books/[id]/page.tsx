3adcfab5e0e1af17d7ea2e7a21e074f858bcdf3c
Klara Formankova <xforma14@vutbr.cz>
2023-09-24 07:33:35 +0200

    Page for book editing

--- src/app/admin/books/[id]/page.tsx
+++ src/app/admin/books/[id]/page.tsx
@@ -1,3 +1,53 @@
+'use client';
+
+import BookForm from "@/app/components/book-form";
+import { IBook } from "@/app/types/global";
+import { useEffect, useState } from "react";
+
 export default function Page({ params }: { params: { id: string } }) {
-    return <div>Book id: {params.id}</div>
+    const [book, setBook] = useState<IBook | null>(null);
+    const [title, setTitle] = useState<string>('');
+    const [author, setAuthor] = useState<string>('');
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+
+    const handleUpdateBook = (updatedBook: IBook) => {
+        setBook(updatedBook);
+        setTitle(updatedBook.title);
+        setAuthor(updatedBook.author);
+        setIsEditing(false);
+    }
+
+    const handleDeleteBook = () => {
+        setBook(null);
+    }
+
+    useEffect(() => {
+        const book: IBook = {
+            id: 1,
+            title: "Book 1",
+            author: "Author 1"
+        };
+        setBook(book);
+        setTitle(book.title);
+        setAuthor(book.author);
+    }, []);
+        
+    return (
+        <div>
+            <br />
+            {book ? (
+                <BookForm
+                    book={book}
+                    title={title}
+                    author={author}
+                    isEditing={isEditing}
+                    setIsEditing={setIsEditing}
+                    onUpdateBook={handleUpdateBook}
+                    onDeleteBook={handleDeleteBook}
+                />
+            ) : (
+                <p>Book not found</p>
+            )}
+        </div>
+    );
 }
