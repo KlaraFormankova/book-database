f1974075609b01e52d57800c4c2d0f20b5d615ef
Klara Formankova <xforma14@vutbr.cz>
2023-09-27 15:54:51 +0200

    Extend book model with language, pages nad desc

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { IBook } from "@/app/types/global";
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import BackToBooksBtn from "@/app/components/back-to-books-btn";
 
 export default function Page({ params }: { params: { id: string } }) {
   const [book, setBook] = useState<IBook | null>(null);
@@ -11,7 +12,7 @@ export default function Page({ params }: { params: { id: string } }) {
   useEffect(() => {
     if (params.id) {
       fetch(
-        `https://crudcrud.com/api/d0565cee39e94d10a4eee4dd3153b12b/books/${params.id}`
+        `https://crudcrud.com/api/db50dacdac654274a04030dec14ad453/books/${params.id}`
       )
         .then((response) => response.json())
         .then((data) => {
@@ -26,30 +27,47 @@ export default function Page({ params }: { params: { id: string } }) {
   return (
     book && (
       <div>
-        <Typography variant="h4" gutterBottom>
-          Book Details
-        </Typography>
-        <br />
+        <div className="flex justify-end mb-3">
+          <BackToBooksBtn />
+        </div>
         <Stack spacing={2} direction={{ xs: "column", sm: "row" }}>
           {book?.image && (
             <Image
               src={book.image}
               alt="Book cover photo"
-              height={233}
+              height={240}
               width={350}
+              className="max-h-96"
             />
           )}
           <Stack spacing={2} direction="column">
             <Stack spacing={2} direction="row">
-              <Typography variant="h5" gutterBottom>
+              <Typography variant="h4" className="-mb-1.5 " gutterBottom>
                 {book.title}
               </Typography>
             </Stack>
             <Stack spacing={2} direction="row">
-              <Typography variant="h6" gutterBottom>
+              <Typography variant="overline" gutterBottom>
                 {book.author}
               </Typography>
             </Stack>
+            <Stack spacing={2} direction="row">
+              <Typography variant="subtitle2" gutterBottom>
+                {book.language}
+              </Typography>
+            </Stack>
+            <Stack spacing={2} direction="row">
+              <Typography variant="subtitle2" gutterBottom>
+                {book.pages} pages
+              </Typography>
+            </Stack>
+            {book.desc && (
+              <Stack spacing={2} direction="row">
+                <Typography variant="body2" gutterBottom>
+                  {book.desc}
+                </Typography>
+              </Stack>
+            )}
           </Stack>
         </Stack>
       </div>
--- src/app/types/global.ts
+++ src/app/types/global.ts
@@ -1,6 +1,9 @@
 export interface IBook {
-    _id?: string;
-    title: string;
-    author: string;
-    image?: string;
-};
+  _id?: string;
+  title: string;
+  author: string;
+  language: string;
+  pages: number;
+  image?: string;
+  desc?: string;
+}
